d146499d24eab107165039a65835175a90c2dfa8
Jonathan Zanella <jonathan.zanella@spartalabs.com.br>
2015-11-01 18:03:03 -0200

    adding DBFlow and Lombok

---
+++ app/src/main/java/br/com/jonathanzanella/myshopping/MyApplication.java
@@ -0,0 +1,17 @@
+package br.com.jonathanzanella.myshopping;
+
+import android.app.Application;
+
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+/**
+ * Created by jonathan on 01/11/15.
+ * Copyright (c) 2015 Sparta Labs. All rights reserved.
+ */
+public class MyApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		FlowManager.init(this);
+	}
+}
